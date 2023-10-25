1b0f4f5eb0caa301bb0a16549c79262a9a343a0a
Bruno Onissanti <brunoonissanti@gmail.com>
2023-10-25 15:10:41 -0300

    Makefile ok

--- src/builtins/main.c
+++ src/builtins/main.c
@@ -12,14 +12,19 @@ int	main(int argc, char **argv, char **envp)
 
 	while (1)
 	{
-		ft_putstr_fd("~$ \n", 1);;
+		ft_putstr_fd("~$ ", 1);;
 		input = get_next_line(0);
 		char **num_args;
 
 		num_args = ft_split(input, ' ');
-		if (strcmp(num_args[0], "export") == 0)
+		if (ft_strcmp(num_args[0], "export\n") == 0)
             ft_export(hash_table, num_args);
         
 		free(input);
+
+		// Free num_args
+		for (int i = 0; num_args[i] != NULL; i++)
+			free(num_args[i]);
+		free(num_args);
 	}
 }
