46e07817eb89138da7ab49cf59b1150a213a6f4c
Bruno Onissanti <brunoonissanti@gmail.com>
2023-11-13 18:09:05 -0300

    checking leaks quotes

--- src/handle_segments/segments_utils.c
+++ src/handle_segments/segments_utils.c
@@ -107,6 +107,23 @@ size_t ft_strcspn(const char *str, char *delim1, char *delim2)
 	return (length);
 }
 
+char	*ft_strndup(const char *str, size_t num)
+{
+	char	*dup;
+	size_t	len;
+
+	len = ft_strlen(str);
+	if (num < len)
+		len = num;
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+	while (*str && num--)
+		*dup++ = *str++;
+	*dup = '\0';
+	return (dup - len);
+}
+
 t_bool	is_whitespace(char c)
 {
 	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\0')
