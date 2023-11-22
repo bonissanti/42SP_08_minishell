2ebcf0779a010cb54a9785a8a7b087310c07cceb
Bruno Rodrigues onissanti <brunrodr@c1r6p3.42sp.org.br>
2023-11-22 11:36:56 -0300

    vector pointer implemented

--- src/execution/check_builtins.c
+++ src/execution/check_builtins.c
@@ -6,7 +6,7 @@
 /*   By: brunrodr <brunrodr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 12:00:37 by brunrodr          #+#    #+#             */
-/*   Updated: 2023/11/21 18:20:17 by brunrodr         ###   ########.fr       */
+/*   Updated: 2023/11/22 11:36:13 by brunrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,77 +14,50 @@
 #include "../include/builtins.h"
 #include "../include/hash.h"
 
-void	cd(char **args)
+
+typedef struct s_command
 {
-	if (strcmp(args[0], "cd") == 0)
-		printf("cd function called\n");
+	char	*name;
+	void	(*function)(char *args);
+}		t_command;
+
+void	echo(char *args)
+{
+	printf("echo function called, %s\n", args);
 }
 
-
-
-void	execute_builtins(t_builtins *pointer, char **args)
+void	export(char *args)
 {
-	pointer->builtins[pointer->index](args);
+	printf("export function called, %s\n", args);
 }
 
-void	export(char **args)
+void	init_builtins(t_command *cmd)
 {
-	if (strcmp(args[0], "export") == 0)
-		printf("export function called\n");
+	cmd[0].name = "echo";
+	cmd[0].function = echo;
+	cmd[1].name = "export";
+	cmd[1].function = export;
 }
 
-
-void	execute_builtin(char *builtin)
+void	execute_builtins(t_command *builtins, char *cmd, char *args)
 {
-	if (strcmp(builtin, "cd") == 0)
-		printf("cd function called\n");
-	else if (strcmp(builtin, "export") == 0)
-		printf("export function called\n");
-	else if (strcmp(builtin, "unset") == 0)
-		printf("unset function called\n");
-	else if (strcmp(builtin, "env") == 0)
-		printf("env function called\n");
-	else if (strcmp(builtin, "exit") == 0)
-		printf("exit function called\n");
-	else if (strcmp(builtin, "pwd") == 0)
-		printf("pwd function called\n");
-	else if (strcmp(builtin, "echo") == 0)
-		printf("echo function called\n");
-	else
-		printf("command not found\n");
+	size_t i;
 
+	i = -1;
+	while (++i < 2)
+	{
+		if (strcmp(cmd, builtins[i].name) == 0)
+		{
+			builtins[i].function(args);
+			return ;
+		}
+	}
 }
-
-// int	find_index(char *builtin)
-// {
-// 	if (strcmp(builtin, "cd") == 0)
-// 		return (0);
-// 	else if (strcmp(builtin, "export") == 0)
-// 		return (1);
-// 	else if (strcmp(builtin, "unset") == 0)
-// 		return (2);
-// 	else if (strcmp(builtin, "env") == 0)
-// 		return (3);
-// 	else if (strcmp(builtin, "exit") == 0)
-// 		return (4);
-// 	else if (strcmp(builtin, "pwd") == 0)
-// 		return (5);
-// 	else if (strcmp(builtin, "echo") == 0)
-// 		return (6);
-// 	else
-// 		return (-1);
-// }
-
 
 int main(void)
 {
-	t_builtins pointer;
-	char *args[2] = {"export", NULL};
-
-	pointer.builtins[0] = cd;
-	pointer.builtins[1] = export;
-	pointer.builtins[2] = NULL;
-	
-	pointer.index = 1;
-	ft_execute_builtins(&pointer, args);
+	char *args[] = {"echo", "otario"};
+	t_command builtins[2];
+	init_builtins(builtins);
+	execute_builtins(builtins, args[0], args[1]);
 }
