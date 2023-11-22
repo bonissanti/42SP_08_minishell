a7c053a513f8d496603fed3f9fd260edfd6e29bb
Bruno Rodrigues onissanti <brunrodr@c2r4p5.42sp.org.br>
2023-11-22 20:01:37 -0300

    analyzing cmd in progress

--- src/execution/prepare_exec.c
+++ src/execution/prepare_exec.c
@@ -6,12 +6,33 @@
 /*   By: brunrodr <brunrodr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 12:07:07 by brunrodr          #+#    #+#             */
-/*   Updated: 2023/11/22 18:28:39 by brunrodr         ###   ########.fr       */
+/*   Updated: 2023/11/22 19:43:06 by brunrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/ast.h"
+#include "../include/temp_ast.h"
 #include "../include/builtins.h"
 #include "../include/hash.h"
 
-// void 
+void	analyzing_cmd(t_hashtable *hashtable, t_temp_ast *node)
+{
+	char *path;
+	
+	if (node)
+	{
+		if (node->type == TYPE_COMMAND)
+		{
+			path = search(hashtable, "PATH")->value;
+			build_cmd_path(path);
+		}
+		else if (node->type == TYPE_REDIRECT && ft_strncmp(node->args, "<<", 2) == 0)
+			handle_heredoc(hashtable, node->delim);
+		else if (node->type == TYPE_REDIRECT && node->args == '<')
+			redirect_input(node->infile);
+		else if (node->type == TYPE_REDIRECT && node->args == '>')
+			redirect_output(node->outfile);
+		else if (node->type == TYPE_REDIRECT && node->args == '>>')
+			redirect_append(node->outfile);
+	}
+}
+
