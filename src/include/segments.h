3c2714a54c863a4b5367e2b673b077aa5558f90e
allfigueiredodev <88257273+allfigueiredodev@users.noreply.github.com>
2023-11-27 16:55:30 -0300

    adding generate_results to work with handle_wildcard func| segments.h updated (#43)

--- src/include/segments.h
+++ src/include/segments.h
@@ -102,5 +102,6 @@ size_t 					ft_strcspn(const char *str, char *delim1);
 void					handle_wildcard(char *pattern);
 char					*ft_strtok(char *str, const char *delim);
 void 					finish_wildcard(t_segment *head, t_file *file);
+char					*generate_results(t_segment *segments, t_file *file);
 
 #endif
